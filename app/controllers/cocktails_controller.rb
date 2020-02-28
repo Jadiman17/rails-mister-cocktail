913d0bb0fa9faf42546b28295791797bf4ba50d0
jawad sarfraz <jadiman@gmail.com>
2020-02-28 14:26:49 +0700

    scss

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,27 +1,33 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
 
-def index
-  @cocktails = Cocktail.all
-end
-
-def show
+  def show
     @cocktail = Cocktail.find(params[:id])
     @doses = @cocktail.doses
-end
+  end
 
-def new
-  @cocktail = Cocktail.new
-end
+  def new
+    @cocktail = Cocktail.new
+  end
 
-def create
+  def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-    redirect_to cocktail_path(@cocktail)
-  else
-    reder 'new'
+      redirect_to cocktail_path(@cocktail)
+    else
+      reder 'new'
+    end
   end
-end
-private
+
+  def destory
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destory
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
 
   def cocktail_params
     params.require(:cocktail).permit(:name)
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,26 +1,33 @@
 class DosesController < ApplicationController
-def new
-  @dose = Dose.new
-  @cocktail = Cocktail.find(params[:cocktail_id])
-end
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
-def show
-   @dose = Dose.find(params[:id])
-end
+  def show
+    @dose = Dose.find(params[:id])
+  end
 
-
-def create
+  def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
-    redirect_to cocktail_path(@cocktail)
-  else
-    render 'new'
-  end
-end
-    private
-    def dose_params
-      params.require(:dose).permit(:description, :ingredient_id)
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
     end
+  end
+
+  def destory
+    @dose = Dose.find(params[:id])
+    @dose.destory
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
